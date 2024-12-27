61ec35fe0506cbf6f565eb8161ac677a410fda3a
Roman-Nazymko <168586482+Roman-Nazymko@users.noreply.github.com>
2024-12-27 17:39:07 +0100

    homework is completed

---
+++ src/js/1-gallery.js
@@ -0,0 +1,32 @@
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
+
+const galleryListEl = document.querySelector('.gallery');
+
+function createItemsMarkup() {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image lazyload"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`
+    )
+    .join('');
+}
+
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryListEl.insertAdjacentHTML('beforeend', imagesMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+});
